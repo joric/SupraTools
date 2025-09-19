1440c1515cae101afa7b648de8af432154c74cdf
joric <joric@users.noreply.github.com>
2025-09-19 18:34:46 +0500

    updated files

--- Scripts/main.lua
+++ Scripts/main.lua
@@ -1,7 +1,7 @@
 local UEHelpers = require("UEHelpers")
 
 local function getHitResult(WorldObject, StartVector, Rotation)
-    local AddValue = UEHelpers.GetKismetMathLibrary():Multiply_VectorInt(UEHelpers.GetKismetMathLibrary():GetForwardVector(Rotation), 60000.0)
+    local AddValue = UEHelpers.GetKismetMathLibrary():Multiply_VectorInt(UEHelpers.GetKismetMathLibrary():GetForwardVector(Rotation), 90000.0)
     local EndVector = UEHelpers.GetKismetMathLibrary():Add_VectorVector(StartVector, AddValue)
     local Color, HitResult = {R=0, G=0, B=0, A=0}, {}
     local WasHit = UEHelpers.GetKismetSystemLibrary():LineTraceSingle(WorldObject, StartVector, EndVector, 0, false, {}, 0, HitResult, true, Color, Color, 0.0)
@@ -14,11 +14,7 @@ function getImpactPoint(WorldObject, StartVector, Rotation)
 end
 
 function getHitObject(WorldObject, StartVector, Rotation)
-    local HitResult = getHitResult(WorldObject, StartVector, Rotation)
-    if not HitResult then return end
-    if UnrealVersion:IsBelow(5,0) then return HitResult.Actor:Get() end
-    if UnrealVersion:IsBelow(5,4) then return HitResult.HitObjectHandle.Actor:Get() end
-    return HitResult.HitObjectHandle.ReferenceObject:Get()
+    return UEHelpers.GetActorFromHitResult(getHitResult(WorldObject, StartVector, Rotation))
 end
 
 require("UnblockEA")
