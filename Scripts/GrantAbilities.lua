98c29cb63344de4d6b129330549166348d89d646
joric <joric@users.noreply.github.com>
2025-09-20 21:20:27 +0500

    updated files

--- Scripts/GrantAbilities.lua
+++ Scripts/GrantAbilities.lua
@@ -1,6 +1,6 @@
 local UEHelpers = require("UEHelpers")
 
-local function grantAbility(InvDefPath)
+local function grantAbilityInternal(InvDefPath)
     local InvMgr = nil
 
     for _, obj in ipairs(FindAllOf("LyraInventoryManagerComponent") or {}) do
@@ -47,47 +47,44 @@ local function grantAbility(InvDefPath)
     end
 end
 
-local function grantAbilities()
-    ExecuteWithDelay(250, function()
-        ExecuteInGameThread(function()
-
-        grantAbility('/Supraworld/Abilities/Walk/Inventory_Walk.Inventory_Walk_C')
-        grantAbility('/Supraworld/Abilities/Crouch/Inventory_Crouch.Inventory_Crouch_C')
-        grantAbility('/Supraworld/Abilities/Run/Inventory_Run.Inventory_Run_C')
-        grantAbility('/Supraworld/Abilities/Jump/Jumps/Inventory_Jump.Inventory_Jump_C')
-        grantAbility('/Supraworld/Abilities/Jump/JumpHeight/Inventory_JumpHeightDouble.Inventory_JumpHeightDouble_C')
-        grantAbility('/Supraworld/Abilities/Strength/Inventory_Strength.Inventory_Strength_C')
-
-        grantAbility('/Supraworld/Abilities/BlowGun/Core/Inventory_BlowGun.Inventory_BlowGun_C')
-
-        grantAbility('/Supraworld/Abilities/Toothpick/Lyra/Inventory_Toothpick.Inventory_Toothpick_C')
-        grantAbility('/Supraworld/Abilities/Toothpick/Upgrades/ToothpickDart/Inventory_Toothpin_Dart.Inventory_Toothpin_Dart_C')
-
-        grantAbility('/Supraworld/Abilities/ThoughtReading/Inventory_ThoughtReading.Inventory_ThoughtReading_C')
-        grantAbility('/Supraworld/Abilities/Ghost/Inventory_ThirdEye.Inventory_ThirdEye_C')
-        grantAbility('/Supraworld/Abilities/Dash/Inventory_Dash.Inventory_Dash_C')
-
-        grantAbility('/Supraworld/Abilities/PlayerMap/Inventory_PlayerMap.Inventory_PlayerMap_C')
-
-        grantAbility('/Supraworld/Abilities/SpongeSuit/Upgrades/Inventory_SpongeSuit.Inventory_SpongeSuit_C')
-
-        -- act 2 abilities
-
-        grantAbility('/Supraworld/Abilities/Spark/Inventory_Spark.Inventory_Spark_C')  -- not sure if works
-
-        grantAbility('/Supraworld/Abilities/LaserWalk/Inventory_LaserWalk.Inventory_LaserWalk_C')
-
-        -- grantAbility('/Supraworld/Abilities/MindControl/Inventory_MindControl.Inventory_MindControl_C') -- unfinished, breaks control/saves
-
-        grantAbility('/Supraworld/Abilities/MindVision/Inventory_MindVision.Inventory_MindVision_C')
-        grantAbility('/Supraworld/Abilities/Shield/Inventory_Shield.Inventory_Shield_C')
-
-        -- misc
-
-        grantAbility('/Supraworld/Abilities/SmellImmunity/Inventory_SmellImmunity.Inventory_SmellImmunity_C') -- doesn't seem player compatible
-        end)
+local function grantAbility(InvDefPath)
+    ExecuteInGameThread(function()
+        grantAbilityInternal(InvDefPath)
     end)
+end
 
+
+local function grantAbilities()
+    grantAbility('/Supraworld/Abilities/Walk/Inventory_Walk.Inventory_Walk_C')
+    grantAbility('/Supraworld/Abilities/Crouch/Inventory_Crouch.Inventory_Crouch_C')
+    grantAbility('/Supraworld/Abilities/Run/Inventory_Run.Inventory_Run_C')
+    grantAbility('/Supraworld/Abilities/Jump/Jumps/Inventory_Jump.Inventory_Jump_C')
+    grantAbility('/Supraworld/Abilities/Jump/JumpHeight/Inventory_JumpHeightDouble.Inventory_JumpHeightDouble_C')
+    grantAbility('/Supraworld/Abilities/Strength/Inventory_Strength.Inventory_Strength_C')
+
+    grantAbility('/Supraworld/Abilities/BlowGun/Core/Inventory_BlowGun.Inventory_BlowGun_C')
+
+    grantAbility('/Supraworld/Abilities/Toothpick/Lyra/Inventory_Toothpick.Inventory_Toothpick_C')
+    grantAbility('/Supraworld/Abilities/Toothpick/Upgrades/ToothpickDart/Inventory_Toothpin_Dart.Inventory_Toothpin_Dart_C')
+
+    grantAbility('/Supraworld/Abilities/ThoughtReading/Inventory_ThoughtReading.Inventory_ThoughtReading_C')
+    grantAbility('/Supraworld/Abilities/Ghost/Inventory_ThirdEye.Inventory_ThirdEye_C')
+    grantAbility('/Supraworld/Abilities/Dash/Inventory_Dash.Inventory_Dash_C')
+
+    grantAbility('/Supraworld/Abilities/PlayerMap/Inventory_PlayerMap.Inventory_PlayerMap_C')
+
+    grantAbility('/Supraworld/Abilities/SpongeSuit/Upgrades/Inventory_SpongeSuit.Inventory_SpongeSuit_C')
+
+    -- act 2 abilities
+    grantAbility('/Supraworld/Abilities/Spark/Inventory_Spark.Inventory_Spark_C')  -- doesn't seem to work
+    grantAbility('/Supraworld/Abilities/LaserWalk/Inventory_LaserWalk.Inventory_LaserWalk_C')
+
+    -- grantAbility('/Supraworld/Abilities/MindControl/Inventory_MindControl.Inventory_MindControl_C') -- unfinished, breaks control/saves
+    grantAbility('/Supraworld/Abilities/MindVision/Inventory_MindVision.Inventory_MindVision_C')
+    grantAbility('/Supraworld/Abilities/Shield/Inventory_Shield.Inventory_Shield_C')
+
+    -- misc
+    grantAbility('/Supraworld/Abilities/SmellImmunity/Inventory_SmellImmunity.Inventory_SmellImmunity_C') -- doesn't seem player compatible
 end
 
 -- experimental, does not work yet
