88f95479ae81a518e5e8736ec98c5f1107fca18f
joric <joric@users.noreply.github.com>
2025-09-22 22:53:54 +0500

    updated files

--- Scripts/SuitRefill.lua
+++ Scripts/SuitRefill.lua
@@ -1,8 +1,41 @@
-local function suitRefill()
+local UEHelpers = require("UEHelpers")
+
+local function suitRefill(color)
     local obj = FindFirstOf("Equippable_SpongeSuit_C")
     if obj and obj:IsValid() then
-        obj:SetCurrentFill(1.0)
+
+        if color then
+            ---@param Instigator AActor
+            ---@param Type LiquidType::Type
+            ---@param Color ESupraColors
+            ---@param VolumeChange double
+            ---function AEquippable_SpongeSuit_C:Liquid(Instigator, Type, Color, VolumeChange) end
+
+            print("Exposing to liquid with color", color, "current color", obj.CurrentColor)
+
+
+            -- see enums: 1 is water, 2 is paint, 3 is oil
+            -- ESupraColors:Whilte is 16
+
+            local pc = UEHelpers.GetPlayerController()
+
+            obj:Liquid(pc, 2, 16, 0.0) -- paint, white. does not work. maybe use real paint volume as instigator
+        else
+            obj:SetCurrentFill(1.0)
+        end
     end
+    return true
 end
 
 RegisterKeyBind(Key.R, suitRefill)
+
+RegisterKeyBind(Key.F, {ModifierKey.CONTROL}, function()
+    suitRefill(1)
+end)
+
+RegisterConsoleCommandHandler("refill", function(FullCommand, Parameters, Ar)
+    if suitRefill(Parameters[1]) then
+        Ar:Log('suit refilled')
+    end
+    return true
+end)
