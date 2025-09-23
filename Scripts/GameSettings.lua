b64d4a1fc9c97e61ded6ded426ddbd119af45c75
joric <joric@users.noreply.github.com>
2025-09-23 20:45:35 +0500

    updated files

---
+++ Scripts/GameSettings.lua
@@ -0,0 +1,35 @@
+local UEHelpers = require("UEHelpers")
+
+RegisterConsoleCommandHandler("gravity", function(FullCommand, Parameters, Ar)
+    local pc = UEHelpers.GetPlayerController()
+    local scale = tonumber(Parameters[1]) or 1.0
+    pc.Pawn.CharacterMovement.GravityScale = scale
+    Ar:Log(string.format("Gravity scale set to %.2f", scale))
+    return true
+end)
+
+
+-- doesn't work for default controller, probably uses Lyta/GAS abilities
+RegisterConsoleCommandHandler("jumpheight", function(FullCommand, Parameters, Ar)
+    local pc = UEHelpers.GetPlayerController()
+    local cm = pc.Pawn.CharacterMovement
+
+    local value = tonumber(Parameters[1])
+    if not value then
+        Ar:Log(string.format("Current JumpZVelocity: %.1f", cm.JumpZVelocity))
+        return true
+    end
+
+    cm.JumpZVelocity = value
+    Ar:Log(string.format("Jump height set (JumpZVelocity = %.1f)", value))
+
+    return true
+end)
+
+
+RegisterConsoleCommandHandler("speed", function(FullCommand, Parameters, Ar)
+    local value = tonumber(Parameters[1]) or 1.0
+    UEHelpers.GetGameplayStatics():SetGlobalTimeDilation(UEHelpers.GetWorld(), value)
+    Ar:Log(string.format("Speed set to %.1f", value))
+    return true
+end)
