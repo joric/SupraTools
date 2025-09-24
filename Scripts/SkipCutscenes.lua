1ec1404841d408c5e21919e7bbe4731f50a03bc3
joric <joric@users.noreply.github.com>
2025-09-24 16:06:15 +0500

    updated files

--- Scripts/SkipCutscenes.lua
+++ Scripts/SkipCutscenes.lua
@@ -41,5 +41,5 @@ end
     -- removeCutscenes() -- disabled, breaks npc animation
 -- end)
 
-RegisterKeyBind(Key.X, {ModifierKey.ALT}, skipCutscene)
+RegisterKeyBind(Key.P, {ModifierKey.ALT}, skipCutscene)
 
