e7d23c4162182ac31a6fd0fa6d3f9f274bd6f04b
joric <joric@users.noreply.github.com>
2025-10-23 15:07:56 +0500

    updated files

--- Scripts/Inventory.lua
+++ Scripts/Inventory.lua
@@ -1,7 +1,5 @@
 -- a newer version of inventory manager, possibly universal for all games
 -- intended to replace grant and deploy commands with add/remove/list commands
--- pc.LyraInventoryManagerComponent and char.LyraEquipmentManagerComponent are nil accessors
--- to get valid ones you have to iterate through attached components
 
 local UEHelpers = require("UEHelpers")
 
@@ -53,6 +51,9 @@ local function tagify(path)
     end
     return name:lower()
 end
+
+-- pc.LyraInventoryManagerComponent and char.LyraEquipmentManagerComponent are nil accessors
+-- to get valid ones you have to iterate through attached components
 
 local function ToggleEquipment(obj, pc, add)
     local eqm = pc.Character:K2_GetComponentsByClass(StaticFindObject('/Script/LyraGame.LyraEquipmentManagerComponent'))[1]:get()
