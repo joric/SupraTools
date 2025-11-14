6aa26f6aec5fcefda1df36a868555f1eb250868a
joric <joric@users.noreply.github.com>
2025-11-14 18:30:29 +0500

    updated files

--- Scripts/UnblockEA.lua
+++ Scripts/UnblockEA.lua
@@ -1,13 +1,12 @@
 local function unblockEA()
     for _, obj in ipairs(FindAllOf("SupraEABlockingVolume_C") or {}) do
         if obj:IsValid() then
-            -- obj:SetActorEnableCollision(false) -- this breaks the game in Timer_StopScriptsTurningOffCollision
+            -- obj:SetActorEnableCollision(false) -- this teleports player to 0,0,0 (Timer_StopScriptsTurningOffCollision)
             local root = obj.K2_GetRootComponent()
             if root and root:IsValid() and root.SetMobility and root.SetMobility:IsValid() then
                 root:SetMobility(2)
                 obj:K2_TeleportTo({X=-100000,Y=-100000,Z=-100000}, {})
             end
-
         end
     end
 end
