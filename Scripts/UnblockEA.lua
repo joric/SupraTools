6f3fb8f913552248f63e457aa71ffc36f64ce3f6
joric <joric@users.noreply.github.com>
2025-11-14 18:45:31 +0500

    updated files

--- Scripts/UnblockEA.lua
+++ Scripts/UnblockEA.lua
@@ -2,7 +2,7 @@ local function unblockEA()
     for _, obj in ipairs(FindAllOf("SupraEABlockingVolume_C") or {}) do
         if obj:IsValid() then
             -- obj:SetActorEnableCollision(false) -- this teleports player to 0,0,0 since 9016 (Timer_StopScriptsTurningOffCollision)
-            obj:K2_DestroyActor()
+            obj:K2_DestroyActor() -- this works
         end
     end
 end
