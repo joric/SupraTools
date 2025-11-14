dfb648d344eaef6c5ad1ef557736fff5fc0c7071
joric <joric@users.noreply.github.com>
2025-11-14 17:36:33 +0500

    updated files

--- Scripts/UnblockEA.lua
+++ Scripts/UnblockEA.lua
@@ -1,3 +1,6 @@
+local UEHelpers = require("UEHelpers")
+local KismetSystem = UEHelpers.GetKismetSystemLibrary()
+
 local cachedEABlockers = {}
 
 local function cacheEA()
@@ -13,32 +16,38 @@ local function unblockEA()
     for _, obj in ipairs(cachedEABlockers) do
         if obj:IsValid() then
             obj:SetActorEnableCollision(false)
+
+            local handle = obj.Timer_StopScriptsTurningOffCollision
+            if handle:IsValid() then
+                -- KismetSystem:K2_ClearTimerHandle(obj, handle) -- crashes, investigate this call
+            end
+
         end
     end
+
 end
 
 RegisterHook("/Script/Engine.PlayerController:ClientRestart", function(self)
     cacheEA()
     unblockEA()
+
+    pcall(function()
+        RegisterHook("/SupraCore/Systems/Volumes/SupraEABlockingVolume.SupraEABlockingVolume_C:StartTimer_StopScriptsTurningOffCollision", function(self)
+            -- print("--- StartTimer_StopScriptsTurningOffCollision called!")
+            return false   -- swallow the restart
+        end)
+    end)
+
+    pcall(function()
+        RegisterHook("/SupraCore/Systems/Volumes/SupraEABlockingVolume.SupraEABlockingVolume_C:Proc_StopScriptsTurningOffCollision", function(self)
+            -- print("--- Proc_StopScriptsTurningOffCollision called!")
+            return false   -- swallow the restart
+        end)
+    end)
+
 end)
 
 LoopAsync(500, unblockEA)
 
---[[
-    --- investigate other methods
-
-    local UEHelpers = require("UEHelpers")
-    local KismetSystem = UEHelpers.GetKismetSystemLibrary()
-
-    local handle = obj.Timer_StopScriptsTurningOffCollision
-    if handle:IsValid() then
-        -- KismetSystem:K2_ClearTimerHandle(obj, handle) -- crashes, investigate this call
-    end
-
-    RegisterHook("/SupraCore/Systems/Volumes/SupraEABlockingVolume.SupraEABlockingVolume_C:StartTimer_StopScriptsTurningOffCollision",
-    function(self)
-        return false   -- swallow the restart
-    end
-]]
-
+RegisterKeyBind(Key.C, unblockEA)
 
