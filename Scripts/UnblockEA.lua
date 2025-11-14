90e08aa4d16a54ee547cfb4065fd5ae33dfc2307
joric <joric@users.noreply.github.com>
2025-11-14 17:10:06 +0500

    updated files

--- Scripts/UnblockEA.lua
+++ Scripts/UnblockEA.lua
@@ -1,5 +1,16 @@
-local function unblockEA()
+local cachedEABlockers = {}
+
+local function cacheEA()
+    cachedEABlockers = {}
     for _, obj in ipairs(FindAllOf("SupraEABlockingVolume_C") or {}) do
+        if obj:IsValid() then
+            table.insert(cachedEABlockers, obj)
+        end
+    end
+end
+
+local function unblockEA()
+    for _, obj in ipairs(cachedEABlockers) do
         if obj:IsValid() then
             obj:SetActorEnableCollision(false)
         end
@@ -7,5 +18,27 @@ local function unblockEA()
 end
 
 RegisterHook("/Script/Engine.PlayerController:ClientRestart", function(self)
+    cacheEA()
     unblockEA()
 end)
+
+LoopAsync(500, unblockEA)
+
+--[[
+    --- investigate other methods
+
+    local UEHelpers = require("UEHelpers")
+    local KismetSystem = UEHelpers.GetKismetSystemLibrary()
+
+    local handle = obj.Timer_StopScriptsTurningOffCollision
+    if handle:IsValid() then
+        -- KismetSystem:K2_ClearTimerHandle(obj, handle) -- crashes, investigate this call
+    end
+
+    RegisterHook("/SupraCore/Systems/Volumes/SupraEABlockingVolume.SupraEABlockingVolume_C:StartTimer_StopScriptsTurningOffCollision",
+    function(self)
+        return false   -- swallow the restart
+    end
+]]
+
+
