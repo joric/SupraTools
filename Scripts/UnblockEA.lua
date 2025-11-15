d32242bc326b6f4b3a908c2a6566ca37bd09b646
joric <joric@users.noreply.github.com>
2025-11-15 15:56:29 +0500

    updated files

--- Scripts/UnblockEA.lua
+++ Scripts/UnblockEA.lua
@@ -3,9 +3,6 @@ local function unblockEA()
         if obj:IsValid() then
             -- obj:SetActorEnableCollision(false) -- this is broken since 9016 (Timer_StopScriptsTurningOffCollision)
             obj:K2_DestroyActor() -- this works
-
-            -- Looks like the issue that teleports player to 0,0,0 is unrelated to this script, probably ue4ss in general
-            -- I disabled all the scripts (including this one) and it still happens. Will investigate.
         end
     end
 end
@@ -14,13 +11,13 @@ RegisterHook("/Script/Engine.PlayerController:ClientRestart", function(self)
     unblockEA()
 end)
 
+-- Looks like the issue that teleports player to 0,0,0 is unrelated to scripting but ue4ss in general
+-- I disabled all the scripts (including this one) and it still happens. This helps to block teleport to 0,0,0
+
 RegisterHook("/Script/Engine.Actor:K2_SetActorLocation", function(self, NewLocation, bSweep, SweepHitResult, bTeleport)
     local vec = NewLocation:get()
     -- print("Actor:", self:get():GetFullName(), "X:", vec.X, "Y:", vec.Y, "Z:", vec.Z)
     if math.abs(vec.X) < 10 and math.abs(vec.Y) < 10 and math.abs(vec.Z) < 10 then
-        -- block teleport to zero
-        return false
+        return false -- block teleport to zero
     end
 end)
-
-
