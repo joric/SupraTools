ffa58b5ef43d997f7a6c396418aad29a6be492b6
joric <joric@users.noreply.github.com>
2025-11-17 19:56:21 +0500

    updated files

--- Scripts/UnblockEA.lua
+++ Scripts/UnblockEA.lua
@@ -62,53 +62,10 @@ local function checkPlayer()
 
 end
 
--- Looks like the issue that teleports player to 0,0,0 is unrelated to scripting but ue4ss in general
-
 RegisterHook("/Script/Engine.PlayerController:ClientRestart", function(self)
     cacheEA()
     unblockEA()
---[[
-    RegisterHook("/Script/Engine.Actor:K2_TeleportTo", function(self, NewLocation, NewRotation)
-        local vec = NewLocation:get()
-        print(string.format("-- hooked K2_TeleportTo %.5f %.5f %.5f %s", vec.X, vec.Y, vec.Z, self:get():GetFullName() ))
-    end)
-
-    RegisterHook("/Script/Engine.Actor:K2_SetActorLocationAndRotation", function(self, NewLocation, NewRotation)
-        local vec = NewLocation:get()
-        print(string.format("-- hooked K2_TeleportTo %.5f %.5f %.5f %s", vec.X, vec.Y, vec.Z, self:get():GetFullName() ))
-    end)
-
-    local loc,rot = {}, {}
-    local teleport = false
-
-    RegisterHook("/Script/Engine.Actor:K2_SetActorLocation", function(self, NewLocation, bSweep, SweepHitResult, bTeleport)
-        local vec = NewLocation:get()
-        local actor = self:get()
-        teleport = false
-        local d = 10
-        if math.abs(vec.X) < d and math.abs(vec.Y) < d and math.abs(vec.Z) < d then
-            loc = actor:K2_GetActorLocation()
-            rot = actor:K2_GetActorRotation()
-            if actor:GetFullName():find('Player_ToyCharacter_C') then
-                print(string.format("--- K2_SetActorLocation %.5f %.5f %.5f (was %.5f %.5f %.5f, %s)", vec.X, vec.Y, vec.Z, loc.X, loc.Y, loc.Z, actor:GetFName():ToString()))
-                NewLocation:Set(loc) -- this doesn't work for some reason
-                teleport = true
-            end
-        end
-    end,
-        function(self, NewLocation, bSweep, SweepHitResult, bTeleport)
-            if teleport then
-                print(string.format("-- teleporting back to %.5f %.5f %.5f --", loc.X, loc.Y, loc.Z))
-                self:get():K2_TeleportTo(loc, rot)
-            end
-        end
-    )
-]]
-
     LoopAsync(1000, checkPlayer)
-
 end)
 
 -- LoopAsync(1000, unblockEA) -- not needed if you K2_ClearTimerHandle on Timer_StopScriptsTurningOffCollision
--- ExecuteWithDelay(500, unblockEA)
-
