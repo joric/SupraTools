a33d71882ae58af0cba3d3ff05c6aebf00caadd8
joric <joric@users.noreply.github.com>
2025-11-17 17:59:20 +0500

    updated files

--- Scripts/UnblockEA.lua
+++ Scripts/UnblockEA.lua
@@ -32,52 +32,6 @@ RegisterHook("/Script/Engine.PlayerController:ClientRestart", function(self)
     cacheEA()
     unblockEA()
 
-    --[[
-    RegisterHook("/Script/Engine.Actor:K2_SetActorLocation", function(self, NewLocation, bSweep, SweepHitResult, bTeleport)
-        local vec = NewLocation:get()
-        local actor = self:get()
-        -- print("Actor:", actor:GetFullName(), "X:", vec.X, "Y:", vec.Y, "Z:", vec.Z)
-        local d = 100
-        if math.abs(vec.X) < d and math.abs(vec.Y) < d and math.abs(vec.Z) < d then
-            -- block teleport to zero
-
-            local loc = actor:K2_GetActorLocation()
-
-            print(string.format("TELEPORTING TO %.5f %.5f %.5f (was %.5f %.5f %.5f, %s)", vec.X, vec.Y, vec.Z, loc.X, loc.Y, loc.Z, actor:GetFName():ToString()))
-
-            -- do something here
-
-            --NewLocation:set({X=loc.X,Y=loc.Y, Z=loc.Z}) -- crashes here
-            bSweep:set(true) -- this doesn't crash but doesn't work either
-
-            ExecuteWithDelay(300, function()
-                --actor:K2_SetActorLocation(loc, false, {}, true)
-                local rot = actor:K2_GetActorRotation()
-                ExecuteInGameThread(function()
-                    --actor:K2_TeleportTo(loc, rot)
-                end)
-            end)
-
-            -- return false -- this doesn't seem to cancel the teleport at all 
-        end
-    end)
-    ]]
-
-    RegisterHook("/Script/Engine.Actor:K2_SetActorLocationAndRotation", function(self, NewLocation, NewRotation)
-        local vec = NewLocation:get()
-        local actor = self:get()
-        local d = 10
-        if math.abs(vec.X) < d and math.abs(vec.Y) < d and math.abs(vec.Z) < d then
-            local loc = actor:K2_GetActorLocation()
-            print(string.format("--- K2_SetActorAndRotation %.5f %.5f %.5f (was %.5f %.5f %.5f, %s)", vec.X, vec.Y, vec.Z, loc.X, loc.Y, loc.Z, actor:GetFName():ToString()))
-            if actor:GetFullName():find('Player_ToyCharacter_C') then
-                print("----- GOT PLAYER, TRYING TO RESET COORDS -----")
-                NewLocation:Set(loc)
-                return false
-            end
-        end
-    end)
-
     RegisterHook("/Script/Engine.Actor:K2_SetActorLocation", function(self, NewLocation, bSweep, SweepHitResult, bTeleport)
         local vec = NewLocation:get()
         local actor = self:get()
