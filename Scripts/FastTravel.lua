f75f0d04dfddcccea6e27dca81a4232228a3054a
joric <joric@users.noreply.github.com>
2025-11-18 06:41:21 +0500

    updated files

--- Scripts/FastTravel.lua
+++ Scripts/FastTravel.lua
@@ -15,39 +15,39 @@ local function fastTravel()
     pc:ClientFlushLevelStreaming()
     pc:ClientForceGarbageCollection()
 
-    ExecuteWithDelay(250, function()
-        ExecuteInGameThread(function()
+    ExecuteInGameThread(function()
 
-            local widget = FindFirstOf("SW_PlayerMapWidget_C")
-            if not widget:IsValid() then
-                widget = FindFirstOf("PlayerMapWidget_C")
-                if not widget:IsValid() then return end
-            end
+        local widget = FindFirstOf("SW_PlayerMapWidget_C")
+        if not widget:IsValid() then
+            widget = FindFirstOf("PlayerMapWidget_C")
+            if not widget:IsValid() then return end
+        end
 
-            local mapActor = FindFirstOf("PlayerMapActor_C")
-            if not mapActor:IsValid() then return end
+        local mapActor = FindFirstOf("PlayerMapActor_C")
+        if not mapActor:IsValid() then return end
 
-            local virtualMap = {}
-            local mapLocation = {}
-            local ok = widget:GetMousePositionOnVirtualMap(virtualMap, mapLocation)
-            if not ok or (virtualMap.X==0 and virtualMap.Y==0) then return end
+        local virtualMap = {}
+        local mapLocation = {}
+        local ok = widget:GetMousePositionOnVirtualMap(virtualMap, mapLocation)
+        if not ok or (virtualMap.X==0 and virtualMap.Y==0) then return end
 
-            local worldX = (mapLocation.X - 0.5) * mapActor.MapWorldSize + mapActor.MapWorldCenter.X
-            local worldY = (mapLocation.Y - 0.5) * mapActor.MapWorldSize + mapActor.MapWorldCenter.Y
+        local worldX = (mapLocation.X - 0.5) * mapActor.MapWorldSize + mapActor.MapWorldCenter.X
+        local worldY = (mapLocation.Y - 0.5) * mapActor.MapWorldSize + mapActor.MapWorldCenter.Y
 
-            local maxHeight = mapActor.CaptureHeight - mapActor.MapWorldCenter.Z + mapActor.MapWorldUpperLeft.Z
+        local maxHeight = mapActor.CaptureHeight - mapActor.MapWorldCenter.Z + mapActor.MapWorldUpperLeft.Z
 
-            local floorHeight = getFloorHeight(pc.Pawn, worldX, worldY, maxHeight)
-            local loc = {X = worldX, Y = worldY, Z = floorHeight + 100}
+        local floorHeight = getFloorHeight(pc.Pawn, worldX, worldY, maxHeight)
+        local loc = {X = worldX, Y = worldY, Z = floorHeight + 100}
 
-            pc.Pawn:K2_TeleportTo(loc, pc.Pawn:K2_GetActorRotation())
+        -- pc.Pawn:K2_TeleportTo(loc, pc.Pawn:K2_GetActorRotation())
 
-            local comp = FindFirstOf("PlayerMapComponent_C")
-            if comp:IsValid() then
-                comp:UpdatePlayerLocationAndFog()
-            end
+        pc.Pawn.RootComponent:K2_SetWorldLocation(loc, false, {}, true) -- safer, doesn't need delay
 
-        end)
+        local comp = FindFirstOf("PlayerMapComponent_C")
+        if comp:IsValid() then
+            comp:UpdatePlayerLocationAndFog()
+        end
+
     end)
 end
 
